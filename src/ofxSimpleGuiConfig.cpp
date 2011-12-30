8bb6bc5c109206b308e340d8fede51e8378b9773
Francesco <francesco@hv-a.com>
2011-12-30 20:25:28 +0100

    reduced panning from 5 to 0 and changed bg color

--- src/ofxSimpleGuiConfig.cpp
+++ src/ofxSimpleGuiConfig.cpp
@@ -40,7 +40,7 @@ ofxSimpleGuiConfig defaultSimpleGuiConfig;
 
 
 ofxSimpleGuiConfig::ofxSimpleGuiConfig() {
-	
+
 	sliderHeight		= 3; // 10
 	colorSliderHeight	= 10; // 10
 	sliderTextHeight	= 36;
@@ -50,24 +50,24 @@ ofxSimpleGuiConfig::ofxSimpleGuiConfig() {
 	slider2DTextHeight	= titleHeight * 1.5;
 	comboBoxHeight		= titleHeight;	//15
 	comboBoxTextHeight	= 15;
-	
-	
-	padding.set			(titleHeight/2, 5); // was titleHeight/2 , 10
+
+
+	padding.set			(titleHeight/2, 0); // was titleHeight/2 , 10
 	offset.set			(titleHeight/2, titleHeight/2);
 	slider2DSize.set	(titleHeight * 4, titleHeight * 4);
-	
+
 	gridSize.x			= 200 + padding.x;
 	gridSize.y			= toggleHeight + padding.y;
-	
+
 	textColor			= 0x888888;
 	textOverColor		= 0xFFFFFF;
-	textBGColor			= 0x000000;
+	textBGColor			= 0x333333;
 	textBGOverColor		= 0x222222;
-	
+
 	fullColor			= 0xaaaaaa;
 	fullOverColor		= 0xffffff;
 	fullActiveColor		= 0x881818;
-	emptyColor			= 0x333333;
-	
+	emptyColor			= 0x111111;
+
 	borderColor			= 0x333333;
 }
