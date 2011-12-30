8ea0285c012920fcfbcdbf6dfcfbfd6733655f14
Francesco <francesco@hv-a.com>
2011-12-30 22:34:37 +0100

    tweaked bg colors

--- src/ofxSimpleGuiConfig.cpp
+++ src/ofxSimpleGuiConfig.cpp
@@ -62,7 +62,7 @@ ofxSimpleGuiConfig::ofxSimpleGuiConfig() {
 	textColor			= 0x888888;
 	textOverColor		= 0xFFFFFF;
 	//textBGColor			= 0x333333;
-	textBGColor.r = textBGColor.g = textBGColor.b = 64;
+	textBGColor.r = textBGColor.g = textBGColor.b = 0;
 	textBGColor.a = 180;
 
 	textBGOverColor		= 0x222222;
@@ -70,7 +70,7 @@ ofxSimpleGuiConfig::ofxSimpleGuiConfig() {
 	fullColor			= 0xaaaaaa;
 	fullOverColor		= 0xffffff;
 	fullActiveColor		= 0x881818;
-	emptyColor.r = emptyColor.g = emptyColor.b = 32;
+	emptyColor.r = emptyColor.g = emptyColor.b = 64;
     emptyColor.a = 180;
 	borderColor			= 0x333333;
 }
